e24a62e6c26eaaf4765c881eb070bab7ba231ef4
Wenxuan Lu <wenxuanlu@hotmail.com>
2024-02-02 12:15:21 -0500

    required functions defined but not implemented

---
+++ R/hiper_glm.R
@@ -0,0 +1,16 @@
+#' High Performance Generalized Linear Model Constructor
+#'
+#' This function creates a high performance GLM model.
+#'
+#' @param design matrix, the design matrix containing covariates for the model
+#' @param outcome vector, the outcome variable for the model
+#'
+#' @return An object of class 'hglm'.
+#'
+#' @export
+#'
+hiper_glm <- function(design, outcome) {
+  warning("The function hiper_glm() is not implemented.")
+  hglm_obj = structure(list(), class="hglm")
+  return(hglm_obj)
+}
---
+++ R/hiper_glm_helper.R
@@ -0,0 +1,35 @@
+#' Coefficients Extraction
+#'
+#' This function extracts the coefficients from a hglm model.
+#'
+#' @param hglm object, an object of class 'hglm'.
+#'
+#' @export
+#'
+coef.hglm <- function(hglm) {
+  warning("The function coef() is not implemented.")
+}
+
+
+#' Variance-Covariance Matrix Calculation
+#'
+#' @param hglm object, an object of class 'hglm'.
+#'
+#' @export
+#'
+vcov.hglm <- function(hglm) {
+  warning("The function vcov() is not implemented.")
+}
+
+
+#' Print Method for hglm
+#'
+#' Displays the hglm object.
+#'
+#' @param hglm object, an object of class 'hglm'.
+#'
+#' @export
+#'
+print.hglm <- function(hglm) {
+  warning("The function print() is not implemented.")
+}
